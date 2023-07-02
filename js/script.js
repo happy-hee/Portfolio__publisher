e7275b21e0d03b8d1830299b8cdc4984906a02fa
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-07-02 18:24:18 +0900

    뉴스 섹션 퍼블리싱

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /**
  * slide
  */
-// 슬라이드 적용
+// 행성 정보 슬라이드
 const planetSwiper = new Swiper(".planet-slide", {
   direction: "horizontal", // 방향
   loop: false, // 반복 여부
@@ -13,6 +13,22 @@ const planetSwiper = new Swiper(".planet-slide", {
   navigation: {
     nextEl: ".planet-slide__next",
     prevEl: ".planet-slide__prev",
-    disabledClass: "planet-slide__disabled",
+    disabledClass: "slide__disabled",
+  },
+});
+
+// 뉴스 슬라이드
+const newsSwiper = new Swiper(".news-slide", {
+  direction: "horizontal", // 방향
+  loop: false, // 반복 여부
+  slidesPerView: "auto", // 슬라이드 width값
+  clickable: true, //클릭
+  allowTouchMove: true, //터치 움직임
+  watchOverflow: true, //마우스 클릭으로 슬라이드 이동
+  // 네비게이션
+  navigation: {
+    nextEl: ".news-slide__next",
+    prevEl: ".news-slide__prev",
+    disabledClass: "slide__disabled",
   },
 });

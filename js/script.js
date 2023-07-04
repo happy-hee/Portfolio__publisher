56d2d7667faf5d3f7c1113f54adbe66155af98a3
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-07-04 17:44:15 +0900

    배너/뉴스 섹션 제거 및 섹션 위치 변경

--- js/script.js
+++ js/script.js
@@ -75,22 +75,6 @@ const planetSwiper = new Swiper(".planet-slide", {
   },
 });
 
-// 뉴스 슬라이드
-const newsSwiper = new Swiper(".news-slide", {
-  direction: "horizontal", // 방향
-  loop: false, // 반복 여부
-  slidesPerView: "auto", // 슬라이드 width값
-  clickable: true, //클릭
-  allowTouchMove: true, //터치 움직임
-  watchOverflow: true, //마우스 클릭으로 슬라이드 이동
-  // 네비게이션
-  navigation: {
-    nextEl: ".news-slide__next",
-    prevEl: ".news-slide__prev",
-    disabledClass: "slide__disabled",
-  },
-});
-
 /**
  * 아코디언 메뉴
  */
